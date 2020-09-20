7c9f9a6168dddcdce8658277a5f8fbf6973202ef
BlackWingedKing <ritesh.goru@gmail.com>
2020-09-20 21:39:06 +0530

    added files

---
+++ excercises/variables/variables5.rs
@@ -0,0 +1,9 @@
+// variables5.rs
+// Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
+
+fn main() {
+    let number = "3"; // don't change this line
+    println!("Number {}", number);
+    let number = 3 as i8;
+    println!("Number {}", number);
+}
